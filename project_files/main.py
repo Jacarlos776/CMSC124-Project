518668023216edfe794742b637f0dc5b5505dd88
Joshua Carlos <jacarlos2@up.edu.ph>
2025-11-12 18:23:24 +0800

    docs: added parser import, fixed comments

--- project_files/main.py
+++ project_files/main.py
@@ -1,4 +1,5 @@
 from lexer import LexicalAnalyzer
+from parser import Parser
 import sys
 
 # -- MAIN --
@@ -19,20 +20,18 @@ row = []
 column = []
 
 # Tokenize and reload of the buffer
-
 try:
     with open(f"{file_path}", "r") as file:
-        # 1. Read the entire file content into a single string
+        # Read the entire file content into a single string
         code = file.read() 
     
-    # 2. Call tokenize only once with the full code string
+    # Call tokenize
     token, lexeme, row, column = Analyzer.tokenize(code)
 
     print("\nRecognized Tokens \n", token)
     print("\nRecognized lexemes \n", lexeme)
 
 except RuntimeError as e:
-    # Handle the mismatch error gracefully
     print(f"Lexical Error: {e}")
     sys.exit(1)
 except FileNotFoundError:
